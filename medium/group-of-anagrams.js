ab170941a2b1e78784bb26ff5c6424df161686d6
Matvei Tarasov <snapoak@yandex.ru>
2022-06-03 09:01:37 +0300

    medium/group-of-anagram

---
+++ medium/group-of-anagrams.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/group-anagrams/
+
+/** 
+ * O(n * k)
+ * I think it can be faster
+*/
+function groupAnagrams(strs) {
+    const result = [];
+    const anagramsStore = {}
+    for (let i = 0; i < strs.length; i++) {
+        const counter = (new Array(26)).fill(1)
+        const word =  strs[i]
+
+        for (let i = 0; i < word.length; i++) {
+            counter[getCharCode(word[i])] += 1
+        }
+
+        const key = counter.join('')
+        if (anagramsStore[key]) {
+            anagramsStore[key].push(strs[i])
+        } else {
+            anagramsStore[key] = [strs[i]]
+        }
+    }
+
+    for (let i in anagramsStore) {
+        result.push(anagramsStore[i])
+    }
+    return result;
+};
+
+function getPrimaryNumber(i) {
+    return ((i ** 2) + i + 41)
+}
+
+function getCharCode(ch) {
+    return ch.charCodeAt(0) - 'a'.charCodeAt(0)
+}
