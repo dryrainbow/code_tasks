a8251f8fca4bcbfca7846cf488b80ec5d8115c45
Matvei Tarasov <snapoak@yandex.ru>
2022-06-06 11:00:53 +0400

    ease/recursion-reverse-linked-list-in-place

---
+++ easy/search-and-sort/binary-search.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/binary-search/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((right - left) / 2) + left;
+        if (target > nums[mid]) {
+            left = mid + 1
+        } else if (target < nums[mid]) {
+            right = mid - 1
+        } else {
+            return mid
+        }
+    }
+    return -1;
+};
