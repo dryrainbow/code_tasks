98d359ce2c938aa6c8e9c38d3e175616775c0e4d
Matvei Tarasov <snapoak@yandex.ru>
2022-06-21 09:40:59 +0400

    medium/insert-interval

--- medium/interval/merge_intervals.py
+++ medium/interval/merge_intervals.py
@@ -1,5 +1,6 @@
-from typing import List
+# https://leetcode.com/problems/merge-intervals/
 
+from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
