b579a85e498fe33a0467c653fb70b3de1e206b44
Matvei Tarasov <snapoak@yandex.ru>
2022-07-02 23:41:27 +0400

    easy/path-sum

---
+++ easy/path-sum.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/path-sum/submissions/
+
+var hasPathSum = function (root, targetSum) {
+    if (!root) return false;
+    if (root.left == null && root.right == null){
+        return root.val === targetSum
+    }
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+};
