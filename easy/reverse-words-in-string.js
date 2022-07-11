a2bfe2d265cf88be9669eadaec6ad79f9c064ccf
dryrainbow <m.tarasov@space307.com>
2022-07-11 20:03:02 +0400

    easy/reverse-word-in-string

---
+++ easy/reverse-words-in-string.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reverseWords = function(s) {
+    return s.split(' ').map(word=>word.split('').reverse().join('')).join(' ')
+};
