65f63c0c976f8cb2ea26b27a82b30e2d6d31b68e
dryrainbow <m.tarasov@space307.com>
2022-07-12 23:21:51 +0400

    easy/middle-of-linked-list

---
+++ easy/middle-of-linked-list.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/middle-of-the-linked-list
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next
+    }
+
+    return slow;
+};
